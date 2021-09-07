59b1168f221c7a75c7a54eed79bb5dc6ceca1e0e
Alaa Osman <alaa.osmana@gmail.com>
2021-09-07 09:38:24 +0200

    log day

--- client/src/components/Prayers/PrayersInput.js
+++ client/src/components/Prayers/PrayersInput.js
@@ -123,6 +123,7 @@ const PrayersCounter = props => {
     // setPrayerCounts(counts);
   };
   const setPrayers = () => {
+    console.log(prayers.day);
     props.setLogs({
       day: prayers.day,
       prayers: { fajr, dhuhr, asr, maghrib, isha }
