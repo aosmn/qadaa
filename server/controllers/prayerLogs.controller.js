68420a58a7d785c6288131830fce18b011a8dcc9
Alaa Osman <alaa.osmana@gmail.com>
2021-09-22 09:43:49 +0200

    added logs

--- server/controllers/prayerLogs.controller.js
+++ server/controllers/prayerLogs.controller.js
@@ -1,14 +1,14 @@
 import Prayers from '../models/prayerLogs.model.js';
 import asyncHandler from 'express-async-handler';
-import {ALL} from '../constants.js'
+import { ALL } from '../constants.js';
 import day from 'dayjs';
 
 // @desc    Get user prayer logs
 // @route   GET /api/prayers/:id
 // @access  Private
 export const getLogs = asyncHandler(async (req, res) => {
-  const prayers = await Prayers.find({ user: req.user._id }).sort({day: -1});
-  res.json({prayers});
+  const prayers = await Prayers.find({ user: req.user._id }).sort({ day: -1 });
+  res.json({ prayers });
 });
 
 // @desc    update user prayer log by day
@@ -16,7 +16,7 @@ export const getLogs = asyncHandler(async (req, res) => {
 // @access  Private
 // body {user, day, prayer, count}
 export const updateLogs = asyncHandler(async (req, res) => {
-  const prayers = await Prayers.updateDay({...req.body, user: req.user._id });
+  const prayers = await Prayers.updateDay({ ...req.body, user: req.user._id });
   res.json(prayers);
 });
 
@@ -25,7 +25,10 @@ export const updateLogs = asyncHandler(async (req, res) => {
 // @access  Private
 // body {user, day, prayer, count}
 export const updateDayPrayers = asyncHandler(async (req, res) => {
-  const prayers = await Prayers.updateDayPrayers({...req.body, user: req.user._id });
+  const prayers = await Prayers.updateDayPrayers({
+    ...req.body,
+    user: req.user._id
+  });
   res.json(prayers);
 });
 
@@ -34,7 +37,11 @@ export const updateDayPrayers = asyncHandler(async (req, res) => {
 // @access  Private
 // body {user, day, count}
 export const updateLogsAllDay = asyncHandler(async (req, res) => {
-  const prayers = await Prayers.updateDay({...req.body, prayer: ALL, user: req.user._id });
+  const prayers = await Prayers.updateDay({
+    ...req.body,
+    prayer: ALL,
+    user: req.user._id
+  });
   res.json(prayers);
 });
 
@@ -43,7 +50,7 @@ export const updateLogsAllDay = asyncHandler(async (req, res) => {
 // @access  Private
 // body {day, prayers}
 export const setDayLogs = asyncHandler(async (req, res) => {
-  const prayers = await Prayers.setDay({...req.body, user: req.user._id });
+  const prayers = await Prayers.setDay({ ...req.body, user: req.user._id });
   res.json(prayers);
 });
 
@@ -51,16 +58,16 @@ export const setDayLogs = asyncHandler(async (req, res) => {
 // @route   GET /api/prayers/day
 // @access  Private
 export const getDayLogs = asyncHandler(async (req, res) => {
-  let query = {}
+  let query = {};
   if (req.query.day) {
     let start = day(req.query.day).startOf('day');
     let end = day(req.query.day).endOf('day');
-
-    query.$and = [{day: {$lte: end }}, {day: {$gte: start}}]
+    query.$and = [{ day: { $lte: end } }, { day: { $gte: start } }];
+    console.log(query);
   }
-  query.user = req.user._id
-  let fullQuery = {...req.query, ...query}
-  delete fullQuery.day
+  query.user = req.user._id;
+  let fullQuery = { ...req.query, ...query };
+  delete fullQuery.day;
   const prayers = await Prayers.findOne(fullQuery);
   res.json(prayers);
 });
@@ -81,7 +88,7 @@ export const getAggregateLogs = asyncHandler(async (req, res) => {
         isha: { $sum: '$isha' },
         total: { $sum: '$total' }
       }
-    },
+    }
   ]);
   res.json(prayers);
 });
