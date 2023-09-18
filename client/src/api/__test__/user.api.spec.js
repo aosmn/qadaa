308ff16155ef831c6a2a5783b7001de98480fd4a
Alaa Osman <alaa.osmana@gmail.com>
2023-09-18 22:01:58 +0200

    finish tests for user.api

--- client/src/api/__test__/user.api.spec.js
+++ client/src/api/__test__/user.api.spec.js
@@ -1,4 +1,12 @@
-import { login, register } from "../user.api";
+import {
+  getMe,
+  login,
+  register,
+  resetPassword,
+  sendPasswordRecoverEmail,
+  updatePrefs,
+  updateUser,
+} from "../user.api";
 import nock from "nock";
 
 describe("user api", () => {
@@ -169,6 +177,245 @@ describe("user api", () => {
 
       expect(spyLocalStorageGet).toHaveBeenCalledWith("user");
       expect(spyLocalStorageGet).toHaveBeenCalledTimes(1);
+    });
+  });
+  describe("getMe", () => {
+    const responseFromServer = {
+      _id: "some-id",
+      name: "Alaa",
+      email: "alaa@test.com",
+      token: "some-token",
+      preferences: {},
+    };
+    it("should make a call to /me to get user profile", async () => {
+      nock("http://localhost:5000", {
+        reqheaders: {
+          "Content-Type": "application/json",
+        },
+      })
+        .get("/api/users/me")
+        .reply(200, responseFromServer);
+
+      const result = await getMe();
+      expect(result).toEqual(responseFromServer);
+    });
+
+    it("should save the response in localStorage", async () => {
+      const spyLocalStorageSet = jest.spyOn(localStorage, "setItem");
+
+      nock("http://localhost:5000", {
+        reqheaders: {
+          "Content-Type": "application/json",
+        },
+      })
+        .get("/api/users/me")
+        .reply(200, responseFromServer);
+
+      const result = await getMe();
+      expect(result).toEqual(responseFromServer);
+
+      expect(spyLocalStorageSet).toHaveBeenCalledWith(
+        "user",
+        JSON.stringify(responseFromServer)
+      );
+      expect(spyLocalStorageSet).toHaveBeenCalledTimes(1);
+    });
+
+    it("should get user from localStorage if the request fails", async () => {
+      const spyLocalStorageGet = jest.spyOn(localStorage, "getItem");
+
+      nock("http://localhost:5000", {
+        reqheaders: {
+          "Content-Type": "application/json",
+        },
+      })
+        .get("/api/users/me")
+        .reply(200);
+
+      await getMe();
+
+      expect(spyLocalStorageGet).toHaveBeenCalledWith("user");
+      expect(spyLocalStorageGet).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("updateUser", () => {
+    it("should make a call to PUT /api/users", async () => {
+      const userUpdate = {
+        email: "alaa@test.com",
+        preferences: {
+          start: new Date().toISOString(),
+        },
+      };
+      nock("http://localhost:5000", {
+        reqheaders: {
+          "Content-Type": "application/json",
+        },
+      })
+        .put("/api/users", userUpdate)
+        .reply(200, {
+          _id: "some-id",
+          name: "Alaa",
+          email: "alaa@test.com",
+          token: "some-token",
+          preferences: { start: new Date() },
+        });
+
+      await updateUser(userUpdate);
+    });
+
+    it("should save the response to localstorage", async () => {
+      const userUpdate = {
+        email: "alaa@test.com",
+        preferences: {
+          start: new Date().toISOString(),
+        },
+      };
+      const response = {
+        _id: "some-id",
+        name: "Alaa",
+        email: "alaa@test.com",
+        token: "some-token",
+        preferences: { start: new Date() },
+      };
+
+      nock("http://localhost:5000", {
+        reqheaders: {
+          "Content-Type": "application/json",
+        },
+      })
+        .put("/api/users", userUpdate)
+        .reply(200, response);
+
+      const spyLocalStorageSet = jest.spyOn(localStorage, "setItem");
+
+      await updateUser(userUpdate);
+
+      expect(spyLocalStorageSet).toHaveBeenCalledWith(
+        "user",
+        JSON.stringify(response)
+      );
+      expect(spyLocalStorageSet).toHaveBeenCalledTimes(1);
+    });
+
+    it("should return user from localstorage if response doesn't return data", async () => {
+      const userUpdate = {
+        email: "alaa@test.com",
+        preferences: {
+          start: new Date().toISOString(),
+        },
+      };
+
+      nock("http://localhost:5000", {
+        reqheaders: {
+          "Content-Type": "application/json",
+        },
+      })
+        .put("/api/users", userUpdate)
+        .reply(200);
+
+      const spyLocalStorageGet = jest.spyOn(localStorage, "getItem");
+
+      await updateUser(userUpdate);
+
+      expect(spyLocalStorageGet).toHaveBeenCalledWith("user");
+      expect(spyLocalStorageGet).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("updatePrefs", () => {
+    it("should make a call to PUT /api/users/prefs", async () => {
+      const userUpdate = {
+        start: new Date().toISOString(),
+      };
+      nock("http://localhost:5000", {
+        reqheaders: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer some-token`,
+        },
+      })
+        .put("/api/users/prefs", userUpdate)
+        .reply(200, {
+          preferences: { start: new Date() },
+        });
+
+      await updatePrefs({ token: "some-token", preferences: userUpdate });
+    });
+
+    it("should save the response to localstorage", async () => {
+      const userUpdate = {
+        start: new Date().toISOString(),
+      };
+      const response = {
+        preferences: { start: new Date() },
+      };
+      nock("http://localhost:5000", {
+        reqheaders: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer some-token`,
+        },
+      })
+        .put("/api/users/prefs", userUpdate)
+        .reply(200, response);
+
+      const spyLocalStorageSet = jest.spyOn(localStorage, "setItem");
+
+      await updatePrefs({ token: "some-token", preferences: userUpdate });
+
+      expect(spyLocalStorageSet).toHaveBeenCalledWith(
+        "user",
+        JSON.stringify(response)
+      );
+      expect(spyLocalStorageSet).toHaveBeenCalledTimes(1);
+    });
+
+    it("should return user from localstorage if response doesn't return data", async () => {
+      const userUpdate = {
+        start: new Date().toISOString(),
+      };
+
+      nock("http://localhost:5000", {
+        reqheaders: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer some-token`,
+        },
+      })
+        .put("/api/users/prefs", userUpdate)
+        .reply(200);
+
+      const spyLocalStorageGet = jest.spyOn(localStorage, "getItem");
+      await updatePrefs({ token: "some-token", preferences: userUpdate });
+
+      expect(spyLocalStorageGet).toHaveBeenCalledWith("user");
+      expect(spyLocalStorageGet).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("sendPasswordRecoverEmail", () => {
+    it("should call POST /api/users/recover", () => {
+      nock("http://localhost:5000", {
+        reqheaders: {
+          "Content-Type": "application/json",
+        },
+      })
+        .put("/api/users/recover", { email: "a@test.com" })
+        .reply(200);
+
+      sendPasswordRecoverEmail("a@test.com");
+    });
+  });
+
+  describe("resetPassword", () => {
+    it("should call POST /api/users/reset", () => {
+      nock("http://localhost:5000", {
+        reqheaders: {
+          "Content-Type": "application/json",
+        },
+      })
+        .put("/api/users/reset", { email: "a@test.com" })
+        .reply(200);
+
+      resetPassword("a@test.com");
     });
   });
 });
--- client/src/api/user.api.js
+++ client/src/api/user.api.js
@@ -1,58 +1,59 @@
-import axios, { setAxiosAuth } from './axiosRequest';
+import axios, { setAxiosAuth } from "./axiosRequest";
 
-export const register = async user => {
+export const register = async (user) => {
   const config = {
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   };
-  const { data } = await axios.post('/api/users', user, config);
+  const { data } = await axios.post("/api/users", user, config);
   if (data) {
-    localStorage.setItem('user', JSON.stringify(data));
+    localStorage.setItem("user", JSON.stringify(data));
   } else {
-    return JSON.parse(localStorage.getItem('user'));
+    return JSON.parse(localStorage.getItem("user"));
   }
   return data;
 };
 
-export const login = async user => {
+export const login = async (user) => {
   const config = {
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   };
-  const { data } = await axios.post('/api/users/login', user, config);
+  const { data } = await axios.post("/api/users/login", user, config);
   if (data) {
-    localStorage.setItem('user', JSON.stringify(data));
-    setAxiosAuth('Bearer ' + data.token);
+    localStorage.setItem("user", JSON.stringify(data));
+    setAxiosAuth("Bearer " + data.token);
   } else {
-    if(localStorage.getItem('user'))
-      return JSON.parse(localStorage.getItem('user'));
+    if (localStorage.getItem("user"))
+      return JSON.parse(localStorage.getItem("user"));
   }
   return data;
 };
 
-export const getMe = async user => {
-  const { data } = await axios.get('/api/users/me');
-  if (data) localStorage.setItem('user', JSON.stringify(data));
+export const getMe = async () => {
+  const { data } = await axios.get("/api/users/me");
+  if (data) localStorage.setItem("user", JSON.stringify(data));
   else {
-    return JSON.parse(localStorage.getItem('user'));
+    const userFromLocalStorage = localStorage.getItem("user");
+    if (userFromLocalStorage) return JSON.parse(userFromLocalStorage);
   }
-  // setAxiosAuth('Bearer ' + data.token)
   return data;
 };
 
-export const updateUser = async user => {
+export const updateUser = async (user) => {
   const config = {
     headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${user.token}`
-    }
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${user.token}`,
+    },
   };
   const { data } = await axios.put(`/api/users`, user, config);
-  if (data) localStorage.setItem('user', JSON.stringify(data));
+  if (data) localStorage.setItem("user", JSON.stringify(data));
   else {
-    return JSON.parse(localStorage.getItem('user'));
+    const userFromLocalStorage = localStorage.getItem("user");
+    if (userFromLocalStorage) return JSON.parse(userFromLocalStorage);
   }
   return data;
 };
@@ -60,43 +61,46 @@ export const updateUser = async user => {
 export const updatePrefs = async ({ token, preferences }) => {
   const config = {
     headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`
-    }
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   };
   const { data } = await axios.put(`/api/users/prefs`, preferences, config);
-  if (data)
+  if (data) {
+    const userFromLocalStorage = localStorage.getItem("user") || "{}";
     localStorage.setItem(
-      'user',
+      "user",
       JSON.stringify({
-        ...JSON.parse(localStorage.getItem('user')),
-        preferences: data.preferences
+        ...JSON.parse(userFromLocalStorage),
+        preferences: data.preferences,
       })
     );
-  else {
-    return JSON.parse(localStorage.getItem('user'));
+  } else {
+    const userFromLocalStorage = localStorage.getItem("user");
+    if(userFromLocalStorage)
+    return JSON.parse(userFromLocalStorage);
   }
   return data;
 };
 
-export const sendPasswordRecoverEmail = async email => {
+export const sendPasswordRecoverEmail = async (email) => {
   const config = {
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   };
 
-  const { data } = await axios.post('/api/users/recover', { email }, config);
+  const { data } = await axios.post("/api/users/recover", { email }, config);
   return data;
 };
 
-export const resetPassword = async user => {
+export const resetPassword = async (user) => {
   const config = {
     headers: {
-      'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   };
 
-  const { data } = await axios.post('/api/users/reset', user, config);
+  const { data } = await axios.post("/api/users/reset", user, config);
   return data;
 };
